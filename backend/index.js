181293b515f2050cdd0a76133f3d0e6bd38f89a2
NesanduD <ndissaka@gmail.com>
2025-11-07 15:52:18 +0530

    Added Book Schema

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,37 @@ import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 const app = express();
+import {Book} from "./models/bookmodel.js";
 
+app.use(express.json());
 
 app.get("/", (request,response) => {
         console.log(request)
         return response.status(234).send("Hello from backend");
 })
 
+app.post('/books', async(request, response) => {
+    try 
+    {
+        if(!request.body.title ||!request.body.author ||!request.body.publishYear)
+            {
+                return response.status(400).send("Missing required book information");
+            }
+        const newBook = 
+            {
+                title: request.body.title,
+                author: request.body.author,
+                publishYear: request.body.publishYear
+            };
+
+            const book = await Book.create(newBook);
+            return response.status(201).send(book);
+        }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send("Error in creating book");
+    }
+});
 
 mongoose.connect(mongoDBURL)
     .then(() => {
