6cb09a4e3de76408b6e0b4c5d6d3254ef8b1c0f9
NesanduD <ndissaka@gmail.com>
2025-11-12 20:17:11 +0530

    Added Search by ID and Update by ID

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 const app = express();
@@ -10,7 +10,7 @@ app.get("/", (request,response) => {
         console.log(request)
         return response.status(234).send("Hello from backend");
 })
-
+// post a book to database
 app.post('/books', async(request, response) => {
     try 
     {
@@ -33,6 +33,58 @@ app.post('/books', async(request, response) => {
         response.status(500).send("Error in creating book");
     }
 });
+// get all books from database
+
+app.get('/books', async(request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            Count: books.length,
+            Data : books
+        });
+    }catch (error) 
+    {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }});
+
+    //Route for get one book by id
+    app.get('/books/:id', async(request, response) => {
+        try {
+            const {id} = request.params;
+            const book = await Book.findById(id);
+            return response.status(200).json(book);
+        }catch (error)
+        {
+            console.log(error.message);
+            response.status(500).send({message: error.message});
+        }
+    });
+
+    // Route for update a book by id
+    app.put('/books/:id', async(request, response) => {
+        try {
+            if (
+                !request.body.title ||
+                !request.body.author ||
+                !request.body.publishYear
+            ) {
+                return response.status(400).send("Missing required book information");
+            };
+            const {id} = request.params;
+            const result = await Book.findByIdAndUpdate(id, request.body);
+
+            if (!result) {
+                return response.status(404).send("Book not found");
+            }
+            
+            return response.status(200).send("Book updated successfully");  
+
+        }catch (error) {
+            console.log(error.message);
+            response.status(500).send({message: error.message});
+        }
+            });
 
 mongoose.connect(mongoDBURL)
     .then(() => {
